19fdb748347161c9dee98daf77bed89ead05b0dc
omphilejmatsobe <omphilejmatsobe@gmail.com>
2024-03-15 02:49:26 +0200

    concat module complet.

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+    Module that takes str1 and a string str2 as
+    arguments and returns a concatenated string
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+        Args:
+            str1: The first string argument
+            str1: The second string argument
+
+        Return:
+            concatenated string
+    """
+
+    return (str1 + str2)
