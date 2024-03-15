7b04fa700b11b63e4e94cf432348bbdf18615c8d
omphilejmatsobe <omphilejmatsobe@gmail.com>
2024-03-15 02:50:34 +0200

    floor module working.

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+    Module with function floor which takes a float n as
+    argument and returns the floor of the float.
+"""
+
+
+def floor(n: float) -> int:
+    """
+        Args:
+            inp: input float number
+
+        Return:
+            float to floor
+    """
+
+    return int(n)
