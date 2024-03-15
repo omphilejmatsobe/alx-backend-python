0beda36f59601133ea0d7551f42e322bb114e8e5
omphilejmatsobe <omphilejmatsobe@gmail.com>
2024-03-15 02:51:30 +0200

    to_str function complete.

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+    Module with function to_str that takes a float n as
+    argument and returns the string representation of the float.
+"""
+
+
+def to_str(n: float) -> str:
+    """
+        Args:
+            n: input float number
+
+        Return:
+            float of type string
+    """
+
+    return str(n)
