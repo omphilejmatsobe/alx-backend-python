01974038fa4ee5cd3622fe53bc17043632980fc5
omphilejmatsobe <omphilejmatsobe@gmail.com>
2024-04-10 23:02:11 +0200

    TestClass updated

---
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+""" Module for testing client """
+
+import json
+import unittest
+from client import GithubOrgClient
+from fixtures import TEST_PAYLOAD
+from unittest.mock import patch, PropertyMock, Mock
+from parameterized import parameterized, parameterized_class
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    """
+    Test that GithubOrgClient.org returns the correct value
+    """
+
+    @parameterized.expand([
+        ('google'),
+        ('abc')
+    ])
+    @patch('client.get_json')
+    def test_org(self, input, mock):
+        """
+        Test that GithubOrgClient.org returns the correct value
+        """
+        test_class = GithubOrgClient(input)
+        test_class.org()
+        mock.assert_called_once_with(f'https://api.github.com/orgs/{input}')
