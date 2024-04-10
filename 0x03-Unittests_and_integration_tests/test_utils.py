bb4ebffa2c45521a0d2a068607b50f78482f7d9c
omphilejmatsobe <omphilejmatsobe@gmail.com>
2024-04-10 22:22:40 +0200

    File Update

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -38,3 +38,24 @@ class TestAccessNestedMap(unittest.TestCase):
         with self.assertRaises(KeyError) as test:
             access_nested_map(nested_map, path)
         self.assertEqual(f"KeyError('{expected}')", repr(test.exception))
+
+
+class TestGetJson(unittest.TestCase):
+    """
+    Test if methord returns for the expected Json
+    """
+
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    def test_get_json(self, test_url, test_payload):
+        """
+        Test that utils.get_json returns the expected result
+        """
+        config = {'return_value.json.return_value': test_payload}
+        patcher = patch('requests.get', **config)
+        mock = patcher.start()
+        self.assertEqual(get_json(test_url), test_payload)
+        mock.assert_called_once()
+        patcher.stop()
