3f40ae6a2c5cb3e5884e659875f759299b1ec960
omphilejmatsobe <omphilejmatsobe@gmail.com>
2024-04-10 22:28:08 +0200

    File Update

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -59,3 +59,32 @@ class TestGetJson(unittest.TestCase):
         self.assertEqual(get_json(test_url), test_payload)
         mock.assert_called_once()
         patcher.stop()
+
+
+class TestMemoize(unittest.TestCase):
+    """
+    Test Memoize
+    """
+
+    def test_memoize(self):
+        """
+        Function for testing memoize
+        """
+
+        class TestClass:
+            """
+            TestClass
+            """
+
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        with patch.object(TestClass, 'a_method') as mock:
+            _class = TestClass()
+            _class.a_property()
+            _class.a_property()
+            mock.assert_called_once()
